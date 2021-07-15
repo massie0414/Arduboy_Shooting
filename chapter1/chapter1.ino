ed689facb0272226756c17bc8fedc8a9483a5aad
massie0414 <massie0414@gmail.com>
2021-07-15 18:17:21 +0900

    動作確認が取れたので、いったんコミット

---
+++ chapter1/chapter1.ino
@@ -0,0 +1,56 @@
+#include <Arduboy2.h>
+
+Arduboy2 arduboy;
+
+class Character {
+public:
+    int x = 0;
+    int y = 0;
+    int w = 0;
+    int h = 0;
+
+    Character(int x, int y, int w, int h);
+};
+
+Character::Character(
+    int xx,
+    int yy,
+    int ww,
+    int hh)
+{
+    x = xx;
+    y = yy;
+    w = ww;
+    h = hh;
+}
+
+const uint8_t bmp_player[8] PROGMEM = {
+    0,16,150,121,121,150,16,0
+};
+
+void setup() {
+  arduboy.begin();
+  arduboy.setFrameRate(15);
+}
+
+void loop() {
+    if (!(arduboy.nextFrame())){
+        return;
+    }
+
+    arduboy.clear();
+
+    Character player(10,28,8,8);
+
+    arduboy.drawBitmap(
+        player.x,
+        player.y,
+        bmp_player,
+        player.w,
+        player.h,
+        WHITE
+    );
+
+    arduboy.display();
+}
+
